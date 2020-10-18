437f7ef17990fb3735be9fdffcf37ec37b41b6f8
ChooChye <37932895+ChooChye@users.noreply.github.com>
2020-10-18 14:45:06 +0800

    Initial Commit

--- qrcode.php
+++ qrcode.php
@@ -1,9 +1,13 @@
 <?php
-//require_once('phpqrcode/qrlib.php');
-//$path = 'img/qrcodes/';
-//$file = $path.uniqid().'.png';
-//$text = 'Something';
-//QRcode::png($text, $file, 'H', 7);
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    require_once('phpqrcode/qrlib.php');
+    $path = 'img/qrcodes/';
+    $file = $path.uniqid().'.png';
+    $text = 'Something';
+    QRcode::png($text, $file, 'H', 7);
+}
+
+
 include('includes/header.php');
 ?>
 
@@ -30,7 +34,7 @@ include('includes/header.php');
                     <div class="col-xl-6 col-lg-5">
                         <div class="card shadow mb-4">
                             <div class="card-body">
-                                <form>
+                                <form actio="" method="post">
                                     <div class="form-group mb-4">
                                         <label><b>Product Category</b></label>
                                         <select class="form-control">
@@ -53,16 +57,21 @@ include('includes/header.php');
                     <div class="col-xl-6 col-lg-5">
                         <div class="card shadow mb-4">
                             <div class="card-body">
-                                <div align="center">
+
+                                <?php
+
+                                if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                                    echo '    <center>
+                                                <img src="img/qrcodes/5f8699bde8152.png"/><br/>
+                                                <a href="#" class="btn btn-primary"><i class="fas fa-print"></i> Print</a>
+                                              </center>';
+                                }else{
+                                    echo '<div align="center">
                                     <div style="border:1px solid #ccc; height:250px; width: 250px; padding:3%">
                                         <p style="padding-top: 25%; text-align: center">Fill up the form to generate a QR code</p>
                                     </div>
-                                </div>
-                                <?php
-//                                    echo '    <center>
-//                                                <img src="img/qrcodes/5f8699bde8152.png"/><br/>
-//                                                <a href="#" class="btn btn-primary"><i class="fas fa-print"></i> Print</a>
-//                                              </center>'
+                                </div>';
+                                }
                                 ?>
                             </div>
                         </div>
